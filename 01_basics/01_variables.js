c9770ed032783f378307323887fbad6b72e01738
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-09-20 15:42:21 +0000

    c2

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,12 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "hikml@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
 // accountId = 2 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "Bengurluru"
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, ])
