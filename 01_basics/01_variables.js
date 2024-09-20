8d6700c474acf62ab621111f13acf06b13362c5c
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-09-20 15:32:31 +0000

    c1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
